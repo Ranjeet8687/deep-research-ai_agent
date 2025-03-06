cdfd9f1f4f73ddb4c6b850216e43e181aad4cd61
Ranjeet Kumar <ranjeetara2003@gmail.com>
2025-03-06 22:48:02 +0530

    Updated .gitignore to ignore .env and modified config.py

--- utils/config.py
+++ utils/config.py
@@ -1,2 +1,10 @@
-TAVILY_API_KEY = "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"
-GEMINI_API_KEY = "AIzaSyA2SNzXHjfaKBLVAmwNz4G5fUR-7GeiuR4"
+from dotenv import load_dotenv
+import os
+
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), "..", ".env"))
+
+TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+if not TAVILY_API_KEY or not GEMINI_API_KEY:
+    raise ValueError("Error: API keys are missing from .env file!")
